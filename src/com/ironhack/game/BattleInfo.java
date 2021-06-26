2379d4443e098bd290000591383f545fb89308a4
MarianaFariaa <marianaefaria@gmail.com>
2021-06-26 15:11:04 +0100

    Menu - first version

---
+++ src/com/ironhack/game/BattleInfo.java
@@ -0,0 +1,19 @@
+package com.ironhack.game;
+
+import com.ironhack.characters.Warrior;
+
+import java.util.ArrayList;
+
+public class BattleInfo {
+
+    private ArrayList<Character> partyPlayer1;
+    private ArrayList<Character> partyPlayer2;
+    private ArrayList<Character> graveyard;
+
+    private int roundNumber;
+
+  //  public void addPlayerToParty1(Character c){
+ //       partyPlayer1.add(c);
+   // }
+
+}
